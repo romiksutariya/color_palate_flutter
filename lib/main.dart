f91140dd1331858b3d4639b52b54fe1849cb38ad
romiksutariya <romiksutariya133@gmail.com>
2023-05-23 10:50:38 +0530

    flutterproject

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+import 'colorPalate.dart';
+
+void main()
+{
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ColorPalate(),
+    ),
+  );
+}
